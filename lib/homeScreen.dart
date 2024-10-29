cb780f3231dc68a89de562d7c4875c4374f0cebf
Suresh S <suthamani51@gmail.com>
2024-10-29 22:39:52 +0530

    developed

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -24,16 +24,19 @@ class homePage extends StatefulWidget {
 }
 
 class _homePageState extends State<homePage> {
+  int page_index = 0;
   @override
   Widget build(BuildContext context) {
     final x = MediaQuery.of(context).size.width;
     final y = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Column(
+      body: 
+      Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Expanded(
-            flex: 1,
+          Container(
+            //flex: 1,
+            height: y * 0.25,
             child: Container(
               alignment: Alignment.center,
               decoration: const BoxDecoration(
@@ -140,18 +143,169 @@ class _homePageState extends State<homePage> {
             ),
           ),
           Expanded(
-            flex: 3,
+            //flex: 3,
             child: Container(
               color: Colors.white,
               child: Column(
-                children: const [
-                  Text('testing'),
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                //crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text('Favourites', style: TextStyle(fontSize: 22, fontWeight: FontWeight.w800, fontFamily: 'Manrope',color:  Color(0xff9754CB))),
+                      SizedBox(width: MediaQuery.of(context).size.width * 0.5),
+                      Text('See All', style: TextStyle(fontSize: 14, fontWeight: FontWeight.w800, fontFamily: 'Manrope', color:  Color(0xff9754CB))),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        //mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: 160,
+                            height: 160,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              color: Color(0xffF8EDFD),
+                            ),
+                            child: Column(
+                              children: [
+                                Image.asset('assets/images/te.png'),
+                                Text('School', style: TextStyle(fontSize: 14, fontWeight: FontWeight.w800, fontFamily: 'Manrope', color:  Color(0xff9754CB))),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            width: 160,
+                            height: 160,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              color: Color(0xffF8EDFD),
+                            ),
+                            child: Column(
 
-                ],
+                              children: [
+                                Image.asset('assets/images/g.png'),
+                                Text('Vegetables', style: TextStyle(fontSize: 14, fontWeight: FontWeight.w800, fontFamily: 'Manrope', color:  Color(0xff9754CB))),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 35),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(width: 160,
+                            height: 160,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              color: Color(0xffF8EDFD),
+                            ),
+                            child: Column(
+
+                              children: [
+                                Image.asset('assets/images/m.png'),
+                                Text('Vegetables', style: TextStyle(fontSize: 14, fontWeight: FontWeight.w800, fontFamily: 'Manrope', color:  Color(0xff9754CB))),
+                              ],
+                            ),
+                          ),
+                          Container(width: 160,
+                            height: 160,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              color: Color(0xffF8EDFD),
+                            ),
+                            child: Column(
+
+                              children: [
+                                Image.asset('assets/images/food.png'),
+                                Text('Vegetables', style: TextStyle(fontSize: 14, fontWeight: FontWeight.w800, fontFamily: 'Manrope', color:  Color(0xff9754CB))),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+
+                    ],
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(),
+                  )
+                
+                ]
               ),
             ),
           ),
         ],
+      ),
+      bottomNavigationBar: NavigationBar(
+        
+        //labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+        animationDuration: const Duration(milliseconds: 200),
+        onDestinationSelected: (int index) {
+          setState(() {
+            page_index = index;
+            if (index == 1) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const Categories(page_index: index,)),
+              );
+            }
+          });
+        },
+        selectedIndex: page_index,
+        indicatorColor: const Color(0xff9754CB),
+        destinations: [
+          NavigationDestination(
+            icon: ImageIcon(AssetImage('assets/images/home.png')),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            
+            icon: ImageIcon(AssetImage('assets/images/cat.png')),
+            label: 'Categories',
+
+          ),
+          NavigationDestination(
+            icon: ImageIcon(AssetImage('assets/images/like.png')),
+            label: 'Favourites',
+          ),
+          NavigationDestination(
+            icon: ImageIcon(AssetImage('assets/images/grp.png')),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Categories extends StatelessWidget {
+  final int page_index;
+  const Categories({Key? key, required this.page_index}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+            
+          },
+          child: const Text('Go back!'),
+        ),
       ),
     );
   }
